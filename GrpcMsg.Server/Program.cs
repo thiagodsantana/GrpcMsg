94c04d8efaf44d6920093b14c1706a3121f71ffd
Thiago Santana <thiago.darley@gmail.com>
2025-03-08 19:44:24 -0300

    Ajuste

--- GrpcMsg.Server/Program.cs
+++ GrpcMsg.Server/Program.cs
@@ -9,6 +9,6 @@ var app = builder.Build();
 app.MapGrpcService<GreeterService>();
 app.MapGrpcService<UsuarioService>();
 
-app.MapGet("/", () => "Servidor gRPC em execução.s");
+app.MapGet("/", () => "Servidor gRPC em execução.");
 
 app.Run();
