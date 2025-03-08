ca12fcbab2bafc5a5358db0b8c4ff2e5e74ffa4c
Thiago Santana <thiago.darley@gmail.com>
2025-03-08 15:46:51 -0300

    Adicionar arquivos de projeto.

---
+++ GrpcMsg.Client/Program.cs
@@ -0,0 +1,51 @@
+﻿using Grpc.Net.Client;
+using ProjetoGrpc;
+
+class Program
+{
+    static async Task Main()
+    {
+        using var channel = GrpcChannel.ForAddress("https://localhost:7044");
+
+        // Criando os clients
+        var greeterClient = new Greeter.GreeterClient(channel);
+        var usuarioClient = new Usuario.UsuarioClient(channel);
+
+        // Chamadas gRPC
+        await EnviarSaudacaoAsync(greeterClient);
+        await EnviarUsuarioAsync(usuarioClient);
+    }
+
+    private static async Task EnviarSaudacaoAsync(Greeter.GreeterClient client)
+    {
+        try
+        {
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "Thiago" });
+            Console.WriteLine($"[Saudação] Resposta do servidor: {reply.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"[Erro - SayHello] {ex.Message}");
+        }
+    }
+
+    private static async Task EnviarUsuarioAsync(Usuario.UsuarioClient client)
+    {
+        try
+        {
+            var request = new UsuarioRequest
+            {
+                Nome = "Thiago Darlei",
+                Email = "thiago.darley@gmail.com",
+                Endereco = "Rua de teste"
+            };
+
+            var reply = await client.EnvioUsuarioAsync(request);
+            Console.WriteLine($"[Usuário] Nome: {reply.Nome} | Email: {reply.Email} | Mensagem: {reply.Mensagem}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"[Erro - EnvioUsuario] {ex.Message}");
+        }
+    }
+}
---
+++ GrpcMsg.Server/Program.cs
@@ -0,0 +1,14 @@
+using ProjetoGrpc.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+app.MapGrpcService<GreeterService>();
+app.MapGrpcService<UsuarioService>();
+
+app.MapGet("/", () => "Servidor gRPC em execução.s");
+
+app.Run();
